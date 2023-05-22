c22be31bfb96cee29227ac88b75fd591fe271ccf
jvnietto <131877936+jvnietto@users.noreply.github.com>
2023-05-22 09:52:49 +0200

    Mise à jour

--- Introduction_Modele.py
+++ Introduction_Modele.py
@@ -1,13 +1,3 @@
 import pandas as pd 
 import numpy as np 
 
-def somme (x) :
-    return x[0] + x[-1]
-
-#Part 1
-def soustraction(x) :
-    return x[0] - x[-1]
-
-#Part 2
-def multiplication(x) :
-    return x[0] * x[-1]
