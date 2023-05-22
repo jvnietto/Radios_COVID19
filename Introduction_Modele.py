7b07fa1b229aa22f62c443247d971fea78f0c677
jvnietto <131877936+jvnietto@users.noreply.github.com>
2023-05-22 10:19:11 +0200

    Update Introduction_Modele.py

--- Introduction_Modele.py
+++ Introduction_Modele.py
@@ -1,3 +1,15 @@
 import pandas as pd 
 import numpy as np 
+import tensorflow as tf
+import glob
 
+#Trouver tous les chemins vers les fichiers qui finissent par .png
+liste = glob.glob('./COVID-19_Radiography_Dataset/*/.png')
+print(liste)
+#Remplacer les \\ par /
+liste = list(map(lambda x : [x, x.split('/')[2]], liste))
+
+#Créer un DataFrame pandas
+df = pd.DataFrame(liste, columns = ['filepath', 'nameLabel'])
+df['label'] = df['nameLabel'].replace(df.nameLabel.unique(), [*range(len(df.nameLabel.unique()))])
+df.head()
